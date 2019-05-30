0d3c5f457d2b5202a3393493f901c6a883bc8043
Ernestas <raudonisernestas@gmail.com>
2019-05-30 13:47:21 +0300

    Implement a recycler view in cities fragment.

--- app/src/main/java/com/raudonikiss/weatherforecast/fragments/CitiesFragment.kt
+++ app/src/main/java/com/raudonikiss/weatherforecast/fragments/CitiesFragment.kt
@@ -6,23 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.raudonikiss.weatherforecast.R
+import com.raudonikiss.weatherforecast.adapters.CitiesAdapter
 import com.raudonikiss.weatherforecast.contracts.CitiesContract
 import com.raudonikiss.weatherforecast.presenters.CitiesPresenter
 import kotlinx.android.synthetic.main.fragment_cities.*
 
 class CitiesFragment : Fragment(), CitiesContract.View {
 
+    //UI
+    private lateinit var mRecyclerView : RecyclerView
+    private lateinit var mViewManager : RecyclerView.LayoutManager
+    private lateinit var mViewAdapter : RecyclerView.Adapter<*>
+    //Variables
     private lateinit var mPresenter : CitiesContract.Presenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_cities, container, false)
-        mPresenter = CitiesPresenter(this)
-        return rootView
+        return inflater.inflate(R.layout.fragment_cities, container, false)
     }
 
     override fun onStart() {
         super.onStart()
+        mPresenter = CitiesPresenter(this)
+        setUpRecyclerView()
         setUpListeners()
     }
 
@@ -30,6 +38,17 @@ class CitiesFragment : Fragment(), CitiesContract.View {
         fb_add_city.setOnClickListener {
             mPresenter.onFloatingButtonClicked()
         }
+    }
+
+    private fun setUpRecyclerView(){
+        mViewManager = LinearLayoutManager(activity)
+        mViewAdapter = CitiesAdapter(listOf())
+
+        mRecyclerView = cities_recycler_view.apply {
+            layoutManager = mViewManager
+            adapter = mViewAdapter
+        }
+
     }
 
     override fun navigateToAddCity() {
